951b7ea8e077aff1d66d5c372857354d3e5c7a03
jaeykim1031 <glorystar1031@gmail.com>
2023-12-13 09:50:10 +0900

    111

---
+++ OracleSQL/08 - DDL, DML.sql
@@ -0,0 +1,14 @@
+/*
+    SQL : 모든 쿼리 구문. select, create, alter, drop, insert, update, delete, grant, revoke, rollbac, commit
+            - 구조화된 질의 언어 
+    
+    
+    
+    
+    DQL
+    DDL
+    DML
+
+
+
+*/
